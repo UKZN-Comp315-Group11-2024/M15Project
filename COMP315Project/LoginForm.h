ea6b899d739750eb3887fb440ae3d1046df872cb
Jaedon Naidu <jaedon.naidu@gmail.com>
2024-04-27 03:23:54 +0200

    created the gui for the face unlock

--- COMP315Project/LoginForm.h
+++ COMP315Project/LoginForm.h
@@ -5,6 +5,7 @@
 #include <string>
 #include "lvl1Form.h"
 #include "playerInfo.h"
+#include "FaceRecognition.h"
 
 namespace M15Namespace {
 
@@ -61,6 +62,8 @@ namespace M15Namespace {
 	private: System::ComponentModel::IContainer^ components;
 	private: System::Windows::Forms::PictureBox^ pictureboxlockgif;
 	private: System::Windows::Forms::Timer^ timerlockgif;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::Label^ label2;
 
 
 
@@ -81,7 +84,8 @@ namespace M15Namespace {
 			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(LoginForm::typeid));
 			this->panelLogin = (gcnew System::Windows::Forms::Panel());
-			this->pictureboxlockgif = (gcnew System::Windows::Forms::PictureBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
 			this->tbSpyName = (gcnew System::Windows::Forms::TextBox());
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->labelloginscroll2 = (gcnew System::Windows::Forms::Label());
@@ -91,13 +95,15 @@ namespace M15Namespace {
 			this->lblWarning = (gcnew System::Windows::Forms::Label());
 			this->lblSuggestor = (gcnew System::Windows::Forms::Label());
 			this->lblSpyName = (gcnew System::Windows::Forms::Label());
+			this->pictureboxlockgif = (gcnew System::Windows::Forms::PictureBox());
 			this->timerloginscroll = (gcnew System::Windows::Forms::Timer(this->components));
 			this->starttypingtimer = (gcnew System::Windows::Forms::Timer(this->components));
 			this->timerlockgif = (gcnew System::Windows::Forms::Timer(this->components));
 			this->panelLogin->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureboxlockgif))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureboxlockgif))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panelLogin
@@ -105,7 +111,8 @@ namespace M15Namespace {
 			this->panelLogin->BackColor = System::Drawing::Color::Transparent;
 			this->panelLogin->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panelLogin.BackgroundImage")));
 			this->panelLogin->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->panelLogin->Controls->Add(this->pictureboxlockgif);
+			this->panelLogin->Controls->Add(this->label2);
+			this->panelLogin->Controls->Add(this->pictureBox3);
 			this->panelLogin->Controls->Add(this->tbSpyName);
 			this->panelLogin->Controls->Add(this->pictureBox2);
 			this->panelLogin->Controls->Add(this->labelloginscroll2);
@@ -115,27 +122,38 @@ namespace M15Namespace {
 			this->panelLogin->Controls->Add(this->lblWarning);
 			this->panelLogin->Controls->Add(this->lblSuggestor);
 			this->panelLogin->Controls->Add(this->lblSpyName);
+			this->panelLogin->Controls->Add(this->pictureboxlockgif);
 			this->panelLogin->ForeColor = System::Drawing::Color::White;
-			this->panelLogin->Location = System::Drawing::Point(-10, -25);
-			this->panelLogin->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->panelLogin->Location = System::Drawing::Point(-8, -20);
 			this->panelLogin->Name = L"panelLogin";
-			this->panelLogin->Size = System::Drawing::Size(1600, 900);
+			this->panelLogin->Size = System::Drawing::Size(1280, 720);
 			this->panelLogin->TabIndex = 3;
 			this->panelLogin->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &LoginForm::panelLogin_Paint);
 			// 
-			// pictureboxlockgif
+			// label2
 			// 
-			this->pictureboxlockgif->BackColor = System::Drawing::Color::Black;
-			this->pictureboxlockgif->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureboxlockgif.Image")));
-			this->pictureboxlockgif->Location = System::Drawing::Point(-345, -298);
-			this->pictureboxlockgif->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->pictureboxlockgif->Name = L"pictureboxlockgif";
-			this->pictureboxlockgif->Size = System::Drawing::Size(2308, 1332);
-			this->pictureboxlockgif->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureboxlockgif->TabIndex = 9;
-			this->pictureboxlockgif->TabStop = false;
-			this->pictureboxlockgif->Visible = false;
-			this->pictureboxlockgif->Click += gcnew System::EventHandler(this, &LoginForm::pictureboxlockgif_Click);
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Courier New", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(413, 617);
+			this->label2->Margin = System::Windows::Forms::Padding(0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(287, 88);
+			this->label2->TabIndex = 11;
+			this->label2->Text = L"OR:\nFacial Recognition";
+			this->label2->Click += gcnew System::EventHandler(this, &LoginForm::label2_Click_1);
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(692, 523);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(154, 182);
+			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox3->TabIndex = 10;
+			this->pictureBox3->TabStop = false;
+			this->pictureBox3->Click += gcnew System::EventHandler(this, &LoginForm::pictureBox3_Click);
 			// 
 			// tbSpyName
 			// 
@@ -144,11 +162,10 @@ namespace M15Namespace {
 			this->tbSpyName->Font = (gcnew System::Drawing::Font(L"Courier New", 90, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->tbSpyName->ForeColor = System::Drawing::Color::White;
-			this->tbSpyName->Location = System::Drawing::Point(522, 228);
-			this->tbSpyName->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tbSpyName->Location = System::Drawing::Point(418, 182);
 			this->tbSpyName->MaxLength = 10;
 			this->tbSpyName->Name = L"tbSpyName";
-			this->tbSpyName->Size = System::Drawing::Size(981, 170);
+			this->tbSpyName->Size = System::Drawing::Size(785, 136);
 			this->tbSpyName->TabIndex = 0;
 			this->tbSpyName->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			this->tbSpyName->TextChanged += gcnew System::EventHandler(this, &LoginForm::tbSpyName_TextChanged);
@@ -158,10 +175,9 @@ namespace M15Namespace {
 			// pictureBox2
 			// 
 			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			this->pictureBox2->Location = System::Drawing::Point(512, 221);
-			this->pictureBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->pictureBox2->Location = System::Drawing::Point(410, 177);
 			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(998, 181);
+			this->pictureBox2->Size = System::Drawing::Size(798, 145);
 			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox2->TabIndex = 8;
 			this->pictureBox2->TabStop = false;
@@ -172,10 +188,9 @@ namespace M15Namespace {
 			this->labelloginscroll2->Font = (gcnew System::Drawing::Font(L"Courier New", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->labelloginscroll2->ForeColor = System::Drawing::Color::DarkGray;
-			this->labelloginscroll2->Location = System::Drawing::Point(501, 625);
-			this->labelloginscroll2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->labelloginscroll2->Location = System::Drawing::Point(401, 453);
 			this->labelloginscroll2->Name = L"labelloginscroll2";
-			this->labelloginscroll2->Size = System::Drawing::Size(821, 67);
+			this->labelloginscroll2->Size = System::Drawing::Size(661, 54);
 			this->labelloginscroll2->TabIndex = 7;
 			this->labelloginscroll2->Text = L"Press <ENTER> to Login";
 			// 
@@ -185,10 +200,9 @@ namespace M15Namespace {
 			this->Loginlabelscroll1->Font = (gcnew System::Drawing::Font(L"Courier New", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->Loginlabelscroll1->ForeColor = System::Drawing::Color::DarkGray;
-			this->Loginlabelscroll1->Location = System::Drawing::Point(-800, 625);
-			this->Loginlabelscroll1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->Loginlabelscroll1->Location = System::Drawing::Point(-642, 453);
 			this->Loginlabelscroll1->Name = L"Loginlabelscroll1";
-			this->Loginlabelscroll1->Size = System::Drawing::Size(821, 67);
+			this->Loginlabelscroll1->Size = System::Drawing::Size(661, 54);
 			this->Loginlabelscroll1->TabIndex = 6;
 			this->Loginlabelscroll1->Text = L"Press <ENTER> to Login";
 			this->Loginlabelscroll1->Click += gcnew System::EventHandler(this, &LoginForm::label2_Click);
@@ -197,19 +211,18 @@ namespace M15Namespace {
 			// 
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(0, 0);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(44, 16);
+			this->label1->Size = System::Drawing::Size(35, 13);
 			this->label1->TabIndex = 5;
 			this->label1->Text = L"label1";
 			// 
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(1430, 732);
+			this->pictureBox1->Location = System::Drawing::Point(1144, 586);
 			this->pictureBox1->Margin = System::Windows::Forms::Padding(2);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(125, 128);
+			this->pictureBox1->Size = System::Drawing::Size(100, 102);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 4;
 			this->pictureBox1->TabStop = false;
@@ -221,10 +234,10 @@ namespace M15Namespace {
 			this->lblWarning->Font = (gcnew System::Drawing::Font(L"Courier New", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->lblWarning->ForeColor = System::Drawing::Color::Red;
-			this->lblWarning->Location = System::Drawing::Point(736, 434);
+			this->lblWarning->Location = System::Drawing::Point(584, 354);
 			this->lblWarning->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->lblWarning->Name = L"lblWarning";
-			this->lblWarning->Size = System::Drawing::Size(460, 65);
+			this->lblWarning->Size = System::Drawing::Size(370, 50);
 			this->lblWarning->TabIndex = 3;
 			this->lblWarning->Text = L"Start typing";
 			this->lblWarning->Click += gcnew System::EventHandler(this, &LoginForm::lblWarning_Click);
@@ -237,10 +250,10 @@ namespace M15Namespace {
 			this->lblSuggestor->Font = (gcnew System::Drawing::Font(L"Courier New", 36, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->lblSuggestor->ForeColor = System::Drawing::Color::White;
-			this->lblSuggestor->Location = System::Drawing::Point(24, 795);
+			this->lblSuggestor->Location = System::Drawing::Point(19, 636);
 			this->lblSuggestor->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->lblSuggestor->Name = L"lblSuggestor";
-			this->lblSuggestor->Size = System::Drawing::Size(0, 65);
+			this->lblSuggestor->Size = System::Drawing::Size(0, 54);
 			this->lblSuggestor->TabIndex = 2;
 			// 
 			// lblSpyName
@@ -249,13 +262,26 @@ namespace M15Namespace {
 			this->lblSpyName->Font = (gcnew System::Drawing::Font(L"Courier New", 56.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->lblSpyName->ForeColor = System::Drawing::Color::White;
-			this->lblSpyName->Location = System::Drawing::Point(72, 258);
+			this->lblSpyName->Location = System::Drawing::Point(58, 206);
 			this->lblSpyName->Margin = System::Windows::Forms::Padding(0);
 			this->lblSpyName->Name = L"lblSpyName";
-			this->lblSpyName->Size = System::Drawing::Size(418, 109);
+			this->lblSpyName->Size = System::Drawing::Size(334, 87);
 			this->lblSpyName->TabIndex = 1;
 			this->lblSpyName->Text = L"Alias:";
 			this->lblSpyName->Click += gcnew System::EventHandler(this, &LoginForm::lblSpyName_Click);
+			// 
+			// pictureboxlockgif
+			// 
+			this->pictureboxlockgif->BackColor = System::Drawing::Color::Black;
+			this->pictureboxlockgif->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureboxlockgif.Image")));
+			this->pictureboxlockgif->Location = System::Drawing::Point(-276, -238);
+			this->pictureboxlockgif->Name = L"pictureboxlockgif";
+			this->pictureboxlockgif->Size = System::Drawing::Size(1846, 1066);
+			this->pictureboxlockgif->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureboxlockgif->TabIndex = 9;
+			this->pictureboxlockgif->TabStop = false;
+			this->pictureboxlockgif->Visible = false;
+			this->pictureboxlockgif->Click += gcnew System::EventHandler(this, &LoginForm::pictureboxlockgif_Click);
 			// 
 			// timerloginscroll
 			// 
@@ -276,9 +302,9 @@ namespace M15Namespace {
 			// 
 			// LoginForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(120, 120);
+			this->AutoScaleDimensions = System::Drawing::SizeF(96, 96);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
-			this->ClientSize = System::Drawing::Size(1580, 851);
+			this->ClientSize = System::Drawing::Size(1264, 681);
 			this->Controls->Add(this->panelLogin);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
@@ -290,9 +316,10 @@ namespace M15Namespace {
 			this->Load += gcnew System::EventHandler(this, &LoginForm::LoginForm_Load);
 			this->panelLogin->ResumeLayout(false);
 			this->panelLogin->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureboxlockgif))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureboxlockgif))->EndInit();
 			this->ResumeLayout(false);
 
 		}
@@ -481,7 +508,7 @@ private: System::Void timerloginscroll_Tick(System::Object^ sender, System::Even
 		Loginlabelscroll1->Visible = true;
 	}
 
-	Loginlabelscroll1->Location = Point(x, 500);
+	Loginlabelscroll1->Location = Point(x, 453);
 	x = p2.X;
 
 	if (x == 1260) {
@@ -499,7 +526,7 @@ private: System::Void timerloginscroll_Tick(System::Object^ sender, System::Even
 		labelloginscroll2->Visible = true;
 	}
 
-	labelloginscroll2->Location = Point(x, 500);
+	labelloginscroll2->Location = Point(x, 453);
 }
 private: System::Void lblSpyName_Click(System::Object^ sender, System::EventArgs^ e) {
 }
@@ -562,6 +589,15 @@ private: System::Void timerlockgif_Tick(System::Object^ sender, System::EventArg
 	this->Hide();
 	lvl1form->ShowDialog();
 	this->Close();
+}
+private: System::Void pictureBox3_Click(System::Object^ sender, System::EventArgs^ e) {
+	FaceRecognition^ facerec = gcnew FaceRecognition();
+	this->Visible = false;
+	this->Hide();
+	facerec->ShowDialog();
+	this->Visible = true;
+}
+private: System::Void label2_Click_1(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
