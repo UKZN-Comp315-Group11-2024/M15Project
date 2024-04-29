249e8508c611ae75e292f9566aa3383c9b18ee13
Neo-Kekana <neokekana07@gmail.com>
2024-04-29 11:50:03 +0200

    Leaderboard errors fixed, picturebox needs to debugged.

--- COMP315Project/Main.cpp
+++ COMP315Project/Main.cpp
@@ -31,11 +31,11 @@ int main(array<String^>^ args) {
 	//lvl1Form lvl1form;
 	//Application::Run(% lvl1form);
 
-	//Leaderboard leaderboard;
-	//Application::Run(% leaderboard);
+	Leaderboard leaderboard;
+	Application::Run(% leaderboard);
 	
-	lvl1Form lvl1form;
-	Application::Run(% lvl1form);
+	//lvl1Form lvl1form;
+	//Application::Run(% lvl1form);
 	
 	/*popup P("Lorem Ipsum is simply dummy text of the printing and typesetting industry, "
 		+   "but also the leap into electronic typesetting, remaining essentially unchanged. "
