7a7fb743adc4c2b95352d903ab48892967b86e9b
Jaedon Naidu <jaedon.naidu@gmail.com>
2024-05-04 04:06:59 +0200

    merge

--- COMP315Project/Main.cpp
+++ COMP315Project/Main.cpp
@@ -29,8 +29,8 @@ int main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	BootForm form;
-	Application::Run(% form);
+	//BootForm form;
+	//Application::Run(% form);
 
 	//LoginForm loginForm;
 	//Application::Run(% loginForm);
@@ -44,8 +44,8 @@ int main(array<String^>^ args) {
 	//lvl3Form lvl3;
 	//Application::Run(% lvl3);
 
-	//lvl4Form lvl4;
-	//Application::Run(% lvl4);
+	lvl4Form lvl4;
+	Application::Run(% lvl4);
 
 	//BonusLevelOne bonusLevelOne;
 	//Application::Run(% bonusLevelOne);
